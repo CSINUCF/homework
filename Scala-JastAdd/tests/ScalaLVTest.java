1669f5415282edbb26320d55bb8ad0178e576419
Bing <bing.rao@outlook.com>
2016-04-19 23:15:17 -0400

    changge build file

--- Scala-JastAdd/tests/ScalaLVTest.java
+++ Scala-JastAdd/tests/ScalaLVTest.java
@@ -7,7 +7,7 @@ public class ScalaLVTest extends TestCase {
 
 	public void testScalaLVTest()
 	{
-		ScalaCompiler compiler = new ScalaCompiler("input/t11.txt");
+		ScalaCompiler compiler = new ScalaCompiler("input/t1.txt");
 		Program p = compiler.run();
 		runPreErrorCheck(p);
 	}
